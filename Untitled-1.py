a39b550540050b439823d0cd9c84f22de68d8fe8
ToksTitiloye <AdetokunboTitiloye@gmail.com>
2025-12-27 06:20:03 +0000

    Update Untitled-1.py
    Learning Operations

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,14 @@
-print("Hello World")
-print("Hello World")
+lessonNumber = 4
+print(lessonNumber)
+lessonNumber = 5
+print(lessonNumber)
+#  5 + 1
+#  6
+lessonNumber = lessonNumber + 1
+print(lessonNumber)
+lessonNumber += 1
+print(lessonNumber)
+
+print(10 % 3)
+print(10 % 2)
+print (11 % 2)
